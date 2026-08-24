b37c3b78c22acf14dc83e22643ed9a8fab1e74c7
Dev Goswami <101167658+builtbyd3v@users.noreply.github.com>
2026-08-24 16:20:24 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,19 @@
+# Input: list of strings (operations), Output: Sum of all scores on the record. 
+
+
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        
+        for op in operations:
+            if op == "+":
+                record.append((record[-1] + record[-2]))
+            elif op == "C":
+                record.pop()
+            elif op =="D":
+                record.append((record[-1] * 2))
+            else:
+                record.append(int(op))
+
+        return sum(record)
