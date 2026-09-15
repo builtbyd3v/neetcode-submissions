2d87008751543be1cb5c9074319b88e87dfcaccf
Dev Goswami <101167658+builtbyd3v@users.noreply.github.com>
2026-09-15 14:30:00 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ss = "".join(sorted(s))
+        st = "".join(sorted(t))
+
+        if ss == st:
+            return True
+        
+        return False
